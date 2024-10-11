483b38d0453ced47faae2bf71a27b641ada89bee
Chalindu Nisal <chaindunisal1998@gmail.com>
2024-10-11 19:49:02 +0530

    Add header and footer

---
+++ application/views/inc/footer.php
@@ -0,0 +1,6 @@
+
+<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+
+</body>
+</html>
---
+++ application/views/inc/header.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>College Managaement System</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>"/>
+	<script src="<?php echo base_url('assets\js\jquery-3.1.0.js'); ?>"></script>
+	<script src="<?php echo base_url('assets\js\bootstrap.min.js'); ?>"></script>
+
+	
+</head>
+<body>
+
+<div id="container">
+
+	<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">COLLEGE MANAGEMENT SYSTEM</a>
+			
+		</div>
+	</nav>
+</div>
